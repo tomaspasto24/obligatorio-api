c3b093bc60aba309d56ba5e0f22cfcc07951994c
tomaspasto24 <tomaspasto24@gmail.com>
2023-06-21 12:18:01 -0300

    rutas++

--- index.js
+++ index.js
@@ -3,60 +3,6 @@ const express = require("express"),
     app = express(),
     puerto = 3000;
 
-app.post('/iniciar-sesion', (peticion, respuesta) => {
-    
-});
-
-app.post('/registrar-usuario', (peticion, respuesta) => {
-    
-});
-
-app.get('/habilidades', (peticion, respuesta) => {
-    
-});
-
-app.get('/habilidades-categorias', (peticion, respuesta) => {
-    
-});
-
-app.get('/solicitudes', (peticion, respuesta) => {
-    
-});
-
-app.get('/solicitudes-relevantes', (peticion, respuesta) => {
-    
-});
-
-app.get('/solicitudes-activas', (peticion, respuesta) => {
-    
-});
-
-app.post('/aceptar-solicitud', (peticion, respuesta) => {
-    
-});
-
-app.post('/solicitud', (peticion, respuesta) => { //crear-solicitud
-    
-});
-
-app.get('/solicitud-chat', (peticion, respuesta) => { 
-    
-});
-
-app.post('/solicitud-chat-mensaje', (peticion, respuesta) => { 
-    
-});
-
-app.post('/finalizar-solicitud', (peticion, respuesta) => { 
-    
-});
-
-app.post('/solicitud-chat-mensaje', (peticion, respuesta) => { 
-    
-});
-
-
-
 // Una vez definidas nuestras rutas podemos iniciar el servidor
 app.listen(puerto, err => {
     if (err) {
---
+++ src/controllers/solicitudes.js
@@ -0,0 +1,51 @@
+app.post('/iniciar-sesion', (peticion, respuesta) => {
+    
+});
+
+app.post('/registrar-usuario', (peticion, respuesta) => {
+    
+});
+
+app.get('/habilidades', (peticion, respuesta) => {
+    
+});
+
+app.get('/habilidades-categorias', (peticion, respuesta) => {
+    
+});
+
+app.get('/solicitudes', (peticion, respuesta) => {
+    
+});
+
+app.get('/solicitudes-relevantes', (peticion, respuesta) => {
+    
+});
+
+app.get('/solicitudes-activas', (peticion, respuesta) => {
+    
+});
+
+app.post('/aceptar-solicitud', (peticion, respuesta) => {
+    
+});
+
+app.post('/solicitud', (peticion, respuesta) => { //crear-solicitud
+    
+});
+
+app.get('/solicitud-chat', (peticion, respuesta) => { 
+    
+});
+
+app.post('/solicitud-chat-mensaje', (peticion, respuesta) => { 
+    
+});
+
+app.post('/finalizar-solicitud', (peticion, respuesta) => { 
+    
+});
+
+app.post('/solicitud-chat-mensaje', (peticion, respuesta) => { 
+    
+});
