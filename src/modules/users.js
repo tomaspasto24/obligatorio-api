388a0ff8be22c80a635123f41e7d74d12312330a
Juan Lupi <juan.lupi@correo.ucu.edu.uy>
2023-06-22 12:15:00 -0300

    getUserById

--- src/modules/users.js
+++ src/modules/users.js
@@ -1,6 +1,6 @@
 const pool = require('../db/connection');
 
-async function getUsers() {
+async function getAllUsers() {
     try {
         const query = 'SELECT * FROM public."Usuario"';
         const result = await pool.query(query);
@@ -11,13 +11,25 @@ async function getUsers() {
     }
 }
 
+async function getUserById(Id) {
+    try {
+        const query = 'SELECT * FROM public."Usuario" WHERE public."Usuario".id = ' + Id;
+        const result = await pool.query(query); 
+        return result.rows;
+    } catch (error) {
+        console.error('Error executing query:', error);
+        throw error;
+    }
+}
+
 async function asd(){
-    const users = await getUsers();
+    const users = await getUserById(78);
     console.log(users); 
 }
 
 asd();
 
 module.exports = {
-    getUsers,
+    getAllUsers,
+    getUserById,
 };
