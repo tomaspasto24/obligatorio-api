91f7fe0f39325885413b5f3d7b4950ffa59fce74
Juan Lupi <juan.lupi@correo.ucu.edu.uy>
2023-06-25 15:13:20 -0300

    habilidades_service done

--- src/data-pg/services/habilidades-service.ts
+++ src/data-pg/services/habilidades-service.ts
@@ -1,13 +1,64 @@
 import { Habilidad } from "../../core/dtos/habilidad";
 import { HabilidadesCategoria } from "../../core/dtos/habilidades-categoria";
 import { IHabilidadesService } from "../../core/services/ihabilidades-service";
+import { IQueryBuilder } from "../../core/patterns/builder/query-builder";
+import { IDatabase } from "../../core/database/idatabase";
+import { Database } from "../database/database";
+import { PGQueryBuilder, and, cons, equal, in_, like, or, prop } from "../patterns/builder/query-builder";
+import { Categoria } from "../../core/models/categoria";
+
 
 export class HabilidadesService implements IHabilidadesService {
-    getHabilidades(): Promise<Habilidad[]> {
-        throw new Error("Method not implemented.");
-    }
-    getHabilidadesCategorias(): Promise<HabilidadesCategoria[]> {
-        throw new Error("Method not implemented.");
+
+    private _database: IDatabase;
+    private _queryBuilder: IQueryBuilder;
+
+    public constructor() {
+        this._database = Database.instance;
+        this._queryBuilder = new PGQueryBuilder();
     }
 
+    public async getHabilidades(): Promise<Habilidad[]> {
+        //Se prepara la consulta
+        let query = this._queryBuilder.select(Habilidad, ['codigo', 'habilidad', 'descripcion', 'codigo_categoria']);
+        query.join(Categoria, 'C', equal(prop('Habilidad', 'codigo_categoria'), prop('C', 'codigo')), ['nombre']);
+
+        //Se ejecuta la consulta
+        let result = await this._database.query(query.build());
+
+        //Se convierte la respuesta a Habilidad
+        let habilidades: Habilidad[] = [];
+        for (let i = 0; i < result.rowCount; i++) {
+            let habilidad: Habilidad = new Habilidad();
+            habilidad.id = result.rows[i].codigo;
+            habilidad.name = result.rows[i].habilidad;
+            habilidad.description = result.rows[i].descripcion;
+            habilidad.categoryId = result.rows[i].codigo_categoria;
+            habilidad.categoryName = result.rows[i].c_nombre
+            habilidades.push(habilidad);
+        }
+
+        //Se retorna habilidades
+        return habilidades;
+    }
+
+    public async getHabilidadesCategorias(): Promise<HabilidadesCategoria[]> {
+        //Se prepara la consulta
+        let query = this._queryBuilder.select(Categoria, ['codigo', 'nombre']);
+
+        //Se ejecuta la consulta
+        let result = await this._database.query(query.build());
+
+        //Se convierte la respuesta a Habilidad
+        let categorias: HabilidadesCategoria[] = [];
+        for (let i = 0; i < result.rowCount; i++) {
+            let categoria: HabilidadesCategoria = new HabilidadesCategoria();
+            categoria.id = result.rows[i].codigo;
+            categoria.name = result.rows[i].nombre;
+            categorias.push(categoria);
+        }
+
+        //Se retorna habilidades
+        return categorias;
+    }
 }
